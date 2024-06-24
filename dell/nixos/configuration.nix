e5a7bd39452a511973bf1e05e16ef06bd42cd7a4
Ibrahim Erturk <me@erturk.me>
2024-06-25 01:43:53 +0300

    Enable autologin

--- dell/nixos/configuration.nix
+++ dell/nixos/configuration.nix
@@ -77,11 +77,11 @@
   services.desktopManager.plasma6.enable = true;
 
   # Enable automatic login for the user.
-  # services.displayManager.autoLogin.enable = true;
-  # services.displayManager.autoLogin.user = "me";
-  # Workaround for autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
-  # systemd.services."getty@tty1".enable = false;
-  # systemd.services."autovt@tty1".enable = false;
+  services.displayManager.autoLogin.enable = true;
+  services.displayManager.autoLogin.user = "me";
+  ### Workaround for autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
+  systemd.services."getty@tty1".enable = false;
+  systemd.services."autovt@tty1".enable = false;
 
   ### Various tests
   # services.xserver.desktopManager.plasma5.enable = true;
