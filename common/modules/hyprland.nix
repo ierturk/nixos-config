659962c62caa4ba7519c6043014de69271926f1f
Ibrahim Erturk <me@erturk.me>
2024-06-26 17:11:18 +0300

    Re-structured directory hierarchy

--- common/modules/hyprland.nix
+++ common/modules/hyprland.nix
@@ -10,6 +10,8 @@ in {
     rofi-wayland
     swww
     waybar
+    pavucontrol
+    wayvnc
   ];
 
   programs.hyprland.enable = true;
@@ -19,7 +21,7 @@ in {
     NIXOS_OZONE_WL = "1";
   };
 
-  fonts.fonts = with pkgs; [
+  fonts.packages = with pkgs; [
     font-awesome
     nerdfonts
   ];
